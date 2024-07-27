6684618c758ecc16661e0011c36bdc00345ae687
JoMaCoBa <correabajo@gmail.com>
2024-07-27 17:55:09 -0600

    Added: themes to Tailwind and create primary-button class

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,37 @@ export default {
         "./resources/**/*.vue",
       ],
   theme: {
-    extend: {},
+    extend: {
+        backgroundColor: theme => ({
+            ...theme('colors'),
+            'primary': '#5a189a',
+            'secondary': '#7b2cbf',
+            'terciary': '#9d4edd'
+          }),
+        borderColor: {
+            'primary': '#5a189a',
+            'secondary': '#7b2cbf',
+            'terciary': '#9d4edd'
+          },
+        textColor: {
+            'primary': '#5a189a',
+            'secondary': '#7b2cbf',
+            'terciary': '#9d4edd'
+        },
+        borderColor: {
+            'primary': '#5a189a',
+            'secondary': '#7b2cbf',
+            'terciary': '#9d4edd'
+          },
+        ringColor: {
+            'primary': '#5a189a',
+            'secondary': '#7b2cbf',
+            'terciary': '#9d4edd'
+        },
+        fontFamily: {
+            'montserrat': ['Montserrat', 'sans-serif']
+          }
+    },
   },
   plugins: [],
 }
