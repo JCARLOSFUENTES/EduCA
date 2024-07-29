9ea76792c7548090987361aaf99d5952ca53a60e
JoMaCoBa <correabajo@gmail.com>
2024-07-29 10:26:21 -0600

    Implement routes for register and route for home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('welcome');
+
+// Ruta de Registro
+// Route::get('/registro', [RegisterController::class, 'index'])->name('register');
+
+// Rutas de Registro
+Route::controller(RegisterController::class)->group(function(){
+
+    Route::get('/registro', 'index')->name('register.index');
+    Route::post('/registro', 'store')->name('register.store');
+
+});
+
+// Ruta Home
+Route::controller(HomeController::class)->group(function(){
+
+    Route::get('/home', 'index')->name('home.index');
+
 });
