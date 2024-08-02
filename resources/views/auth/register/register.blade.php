d4e2b0544aee30acfff114e285e8dbc5fdcdda95
JoMaCoBa <correabajo@gmail.com>
2024-08-01 23:01:20 -0600

    Upgrade UI for register

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -3,71 +3,70 @@
 @section('title', 'EduCA - Registro')
 
 @section('content')
-    <div class="flex flex-col justify-center sm:h-screen p-4">
-        <div class="max-w-md w-full mx-auto rounded-2xl p-8 shadow-lg border border-slate-300">
-            <div class="text-center mb-12">
-                <a href="{{route('welcome')}}">
-                    <x-logo></x-logo>
-                </a>
-            </div>
 
-            <form action="{{route('register.store')}}" method="POST" novalidate>
+<main class="flex min-h-screen bg-gray-100">
+
+    <div class="w-full min-h-screen flex items-center justify-center relative bg-register">
+        <div class="absolute inset-0 bg-black opacity-50 z-0"></div> <!-- Overlay oscuro -->
+
+        <div class="w-full max-w-md h-auto p-6 bg-background md:rounded-lg shadow-md z-10 md:border-2 md:border-black overflow-hidden">
+            <form action="{{route('register.store')}}" method="POST" class="w-full">
+
                 @csrf
                 @method('POST')
-                <div class="space-y-3">
 
-                    <div>
-                        <label for="name" class="text-gray-800 text-sm mb-2 block font-semibold">Nombre</label>
-                        <input id="name" name="name" type="text" class="form-inputs @error('name') border-red-500 @enderror" placeholder="Juan Carlos Rodriguez" value="{{old('name')}}"/>
-                        @error('name')
-                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
-                                {{$message}}
-                            </p>
-                        @enderror
-                    </div>
-
-                    <div>
-                        <label for="username" class="text-gray-800 text-sm mb-2 block font-semibold">Usuario</label>
-                        <input id="username" name="username" type="text" class="form-inputs @error('username') border-red-500 @enderror" placeholder="@JuanCa" value="{{old('username')}}"/>
-                        @error('username')
-                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
-                        @enderror
-                    </div>
-
-                    <div>
-                        <label for="email" class="text-gray-800 text-sm mb-2 block font-semibold">Correo Electronico</label>
-                        <input id="email" name="email" type="email" class="form-inputs @error('email') border-red-500 @enderror" placeholder="juca@example.com" value="{{old('email')}}"/>
-                        @error('email')
-                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
-                        @enderror
-                    </div>
-
-                    <div>
-                        <label for="password" class="text-gray-800 text-sm mb-2 block font-semibold">Contraseña</label>
-                        <input id="password" name="password" type="password" class="form-inputs @error('password') border-red-500 @enderror" placeholder="*******" />
-                        @error('password')
-                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
-                        @enderror
-                    </div>
-
-                    <div>
-                        <label for="password_confirmation" class="text-gray-800 text-sm mb-2 block font-semibold">Repetir Contraseña</label>
-                        <input id="password_confirmation" name="password_confirmation" type="password" class="form-inputs @error('password_confirmation') border-red-500 @enderror" placeholder="*******" />
-                        @error('password_confirmation')
-                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
-                        @enderror
-                    </div>
-
-                    <div class="!mt-12">
-                        <input type="submit" value="Crear Cuenta" class="form-button">
-                        </input>
-                    </div>
-
-                    <p class="text-gray-800 text-sm mt-6 text-center">Ya tienes una cuenta <a href="{{route('login.index')}}" class="text-secondary font-semibold hover:underline ml-1">Inicia Sesión aquí</a></p>
-
+                <div class="flex justify-between items-center mb-6">
+                    <a href="{{route('welcome')}}" class="text-4xl font-bold text-gray-800 text-center">EduCA</a>
+                    <a href="{{route('login.index')}}" class="text-gray-800 underline">Iniciar Sesión</a>
                 </div>
 
+                <div class="mb-4">
+                    <label for="name" class="block text-gray-800 text-lg font-medium mb-2">Nombre</label>
+                    <input type="text" name="name" id="name" class="!w-full form-inputs" placeholder="John Doe">
+                    @error('name')
+                        <p class="message-error mt-2">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label for="username" class="block text-gray-800 text-lg font-medium mb-2">Usuario</label>
+                    <input type="text" name="username" id="username" class="!w-full form-inputs" placeholder="JohnD">
+                    @error('username')
+                        <p class="message-error mt-2">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label for="email" class="block text-gray-800 text-lg font-medium mb-2">Correo Electrónico</label>
+                    <input type="email" name="email" id="email" class="!w-full form-inputs" placeholder="email@example.com">
+                    @error('email')
+                        <p class="message-error mt-2">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label for="password" class="block text-gray-800 text-lg font-medium mb-2">Contraseña</label>
+                    <input type="password" name="password" id="password" class="!w-full form-inputs" placeholder="******">
+                    @error('password')
+                        <p class="message-error mt-2">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label for="password_confirmation" class="block text-gray-800 text-lg font-medium mb-2">Repetir Contraseña</label>
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="!w-full form-inputs" placeholder="******">
+                    @error('password_confirmation')
+                        <p class="message-error mt-2">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <div>
+                    <input type="submit" value="Registrarse" class="!w-full button-primary cursor-pointer">
+                </div>
             </form>
         </div>
     </div>
+
+</main>
+
 @endsection
